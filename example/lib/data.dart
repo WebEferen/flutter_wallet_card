9d0b7d5b800fde48a17cd21a4655c165cef010c9
nimr77 <nimr.h.sawafta@gmail.com>
2023-05-11 09:41:05 +0100

    remove stuff

--- example/lib/data.dart
+++ example/lib/data.dart
@@ -1,4 +1,3 @@
 List<String> get data => [
-      "https://storage.cloud.google.com/ticketma-dev.appspot.com/passes/10911/6923491/pass_0.pkpass",
-      "https://storage.cloud.google.com/ticketma-dev.appspot.com/passes/10911/6950050/pass_0.pkpass"
+// add links here
     ];
