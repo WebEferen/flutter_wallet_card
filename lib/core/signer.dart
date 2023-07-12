887507ad158c214fa98dcab628803e8e81e14cbe
Michal Makowski <michal_makowski@external.mckinsey.com>
2023-07-12 21:22:40 +0200

    Added signer

---
+++ lib/core/signer.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+import 'package:cli_script/cli_script.dart';
+import 'package:flutter/services.dart';
+
+class Signer {
+  static Directory directory = Directory('certificates');
+
+  final File wwdrPem;
+  final File certificateP12;
+
+  final File certPem = File('${directory.path}/cert.pem');
+  final File keyPem = File('${directory.path}/key.pem');
+  final File signature = File('${directory.path}/signature');
+
+  Signer({
+    required this.wwdrPem,
+    required this.certificateP12,
+  }) {
+    if (!wwdrPem.existsSync()) throw Exception('Missing wwdr.pem');
+    if (!certificateP12.existsSync()) throw Exception('Missing cert.p12');
+
+    isOpenSSLSupported().then((isSupported) =>
+        {if (!isSupported) throw Exception('OpenSSL is not supported!')});
+  }
+
+  static Future<bool> isOpenSSLSupported() async {
+    final response = await output('which openssl');
+    return (response.isNotEmpty) ? true : false;
+  }
+
+  Future<void> generateCertificate() async {
+    final arguments = [
+      '-in "${certificateP12.path}"',
+      '-clcerts',
+      '-nokeys',
+      '-out "${certPem.path}"',
+      '-passin pass:'
+    ];
+
+    await run('openssl pkcs12 ${arguments.join(' ')}');
+  }
+
+  Future<void> generateKey({String password = '12345'}) async {
+    final arguments = [
+      '-in "${certificateP12.path}"',
+      '-nocerts',
+      '-out "${keyPem.path}"',
+      '-passin pass:',
+      '-passout pass:$password'
+    ];
+
+    await run('openssl pkcs12 ${arguments.join(' ')}');
+  }
+
+  Future<void> generateSignature({
+    required File manifest,
+    String password = '12345',
+  }) async {
+    if (!certPem.existsSync()) throw Exception('Missing cert.pem');
+    if (!keyPem.existsSync()) throw Exception('Missing key.pem');
+
+    final arguments = [
+      '-in "${manifest.path}"',
+      '-out "${signature.path}"',
+      '-certfile "${wwdrPem.path}"',
+      '-signer "${certPem.path}"',
+      '-inkey "${keyPem.path}"',
+      '-passin pass:$password',
+      '-outform DER',
+      '-binary',
+      '-sign',
+    ];
+
+    await run('openssl smime ${arguments.join(' ')}');
+  }
+
+  Uint8List getSignature() {
+    final signature = File('${directory.path}/signature');
+    if (!signature.existsSync()) throw Exception('Signature file is missing!');
+
+    return signature.readAsBytesSync();
+  }
+}
---
+++ test/signer_test.dart
@@ -0,0 +1,125 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_wallet_card/core/signer.dart';
+
+void main() {
+  final fixtures = Directory('test/fixtures');
+  final certificates = Directory('${fixtures.path}/certificates');
+
+  final certP12 = File('${certificates.path}/cert.p12');
+  final wwdrPem = File('${certificates.path}/wwdr.pem');
+  final signature = File('${certificates.path}/signature');
+
+  group('Signer', () {
+    group('isOpenSSLSupported', () {
+      test('should return true when supported', () async {
+        final isSupported = await Signer.isOpenSSLSupported();
+        expect(isSupported, true);
+      });
+    });
+
+    group('instantiate class', () {
+      test('should throw when cert does not exists', () {
+        final File certP12 = File('${certificates.path}/missing.p12');
+        final File wwdrPem = File('${certificates.path}/wwdr.pem');
+
+        expect(
+          () => Signer(wwdrPem: wwdrPem, certificateP12: certP12),
+          throwsException,
+        );
+      });
+
+      test('should throw when wwdr does not exists', () {
+        final File certP12 = File('${certificates.path}/cert.p12');
+        final File wwdrPem = File('${certificates.path}/missing.pem');
+
+        expect(
+          () => Signer(wwdrPem: wwdrPem, certificateP12: certP12),
+          throwsException,
+        );
+      });
+
+      test('should create class', () {
+        final signer = Signer(wwdrPem: wwdrPem, certificateP12: certP12);
+
+        expect(signer.certificateP12.path, certP12.path);
+        expect(signer.wwdrPem.path, wwdrPem.path);
+      });
+    });
+
+    group('getSignature', () {
+      test('should throw when no signature generated', () {
+        final signer = Signer(wwdrPem: wwdrPem, certificateP12: certP12);
+        expect(() => signer.getSignature(), throwsException);
+      });
+
+      test('should return generated signature', () {
+        Signer.directory = certificates;
+        final signer = Signer(wwdrPem: wwdrPem, certificateP12: certP12);
+
+        signature..createSync(recursive: true);
+        expect(signer.getSignature(), []);
+
+        signature.deleteSync(recursive: true);
+      });
+    });
+
+    group('generators', () {
+      test('should generate certificate', () async {
+        Signer.directory = certificates;
+        final signer = Signer(wwdrPem: wwdrPem, certificateP12: certP12);
+
+        await signer.generateCertificate();
+        expect(signer.certPem.existsSync(), true);
+
+        signer.certPem.deleteSync(recursive: true);
+      });
+
+      test('should generate key', () async {
+        Signer.directory = certificates;
+        final signer = Signer(wwdrPem: wwdrPem, certificateP12: certP12);
+
+        await signer.generateKey();
+        expect(signer.keyPem.existsSync(), true);
+
+        signer.keyPem.deleteSync(recursive: true);
+      });
+    });
+
+    group('generateSignature', () {
+      test('should throw when cert.pem is missing', () async {
+        Signer.directory = certificates;
+        final signer = Signer(wwdrPem: wwdrPem, certificateP12: certP12);
+
+        final manifest = File('${certificates.path}/manifest')
+          ..createSync(recursive: true)
+          ..writeAsStringSync('{}');
+
+        expect(signer.generateSignature(manifest: manifest), throwsException);
+        expect(signer.signature.existsSync(), false);
+        manifest.deleteSync(recursive: true);
+      });
+
+      test('should generate signature', () async {
+        Signer.directory = certificates;
+        final signer = Signer(wwdrPem: wwdrPem, certificateP12: certP12);
+
+        final manifest = File('${certificates.path}/manifest')
+          ..createSync(recursive: true)
+          ..writeAsStringSync('{}');
+
+        await signer.generateKey();
+        await signer.generateCertificate();
+        await signer.generateSignature(manifest: manifest);
+
+        expect(signer.signature.existsSync(), true);
+
+        signer.signature.deleteSync(recursive: true);
+        signer.certPem.deleteSync(recursive: true);
+        signer.keyPem.deleteSync(recursive: true);
+        manifest.deleteSync(recursive: true);
+      });
+    });
+  });
+}
