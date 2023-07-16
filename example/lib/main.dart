3e389c79de55ee3aa2e37693e7184804f0348689
Michal Makowski <michal_makowski@external.mckinsey.com>
2023-07-16 18:57:47 +0200

    Final fixes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_wallet_card/core/passkit.dart';
-import 'package:flutter_wallet_card/flutter_wallet_card.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter_wallet_card_example/pass.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -27,29 +22,23 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Apple wallet cards example (using links)'),
+              const Text('Apple wallet cards using URL'),
+              const SizedBox(height: 5),
               CupertinoButton.filled(
-                  onPressed: () {
-                    _generateWalletPassFromPath();
-                  },
-                  child: Text('Add apple pass'))
+                onPressed: () => generateWalletPassFromUri(exampleUrl),
+                child: Text('Add apple pass'),
+              ),
+              const SizedBox(height: 20),
+              const Text('Apple wallet cards using File Path'),
+              const SizedBox(height: 5),
+              CupertinoButton.filled(
+                onPressed: () => generateWalletPassFromPath(),
+                child: Text('Add apple pass (from path)'),
+              ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  _generateWalletPassFromPath() async {
-    final directory = await getTemporaryDirectory();
-    final file = File('${directory.path}/example.pkpass');
-
-    final examplePass = await rootBundle.load('passes/example.pkpass');
-    final written = await file.writeAsBytes(examplePass.buffer.asUint8List());
-
-    final passkit = await Passkit().saveFromPath(id: 'example', file: written);
-    await FlutterWalletCard.addPasskit(passkit);
-
-    await file.delete();
   }
 }
--- example/lib/pass.dart
+++ example/lib/pass.dart
@@ -1,9 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
+import 'package:flutter_wallet_card/core/passkit.dart';
+import 'package:flutter_wallet_card/flutter_wallet_card.dart';
 import 'package:flutter_wallet_card/models/PasskitField.dart';
 import 'package:flutter_wallet_card/models/PasskitPass.dart';
 import 'package:flutter_wallet_card/models/PasskitStructure.dart';
+import 'package:path_provider/path_provider.dart';
 
-final ExamplePass = PasskitPass(
+void generateWalletPassFromUri(String url) async {
+  final passkit = await Passkit().saveFromUri(id: 'example', url: url);
+  await FlutterWalletCard.addPasskit(passkit);
+}
+
+void generateWalletPassFromPath() async {
+  final directory = await getTemporaryDirectory();
+  final file = File('${directory.path}/example.pkpass');
+
+  final examplePass = await rootBundle.load('passes/example.pkpass');
+  final written = await file.writeAsBytes(examplePass.buffer.asUint8List());
+
+  final passkit = await Passkit().saveFromPath(id: 'example', file: written);
+  await FlutterWalletCard.addPasskit(passkit);
+
+  await file.delete();
+}
+
+final exampleUrl =
+    'https://github.com/WebEferen/flutter_wallet_card/raw/v3/example/passes/example.pkpass';
+
+final examplePass = PasskitPass(
   description: 'Some description',
   formatVersion: 1,
   organizationName: 'Michal Makowski',
